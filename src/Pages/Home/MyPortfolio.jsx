77dd9d11626906567d003a76adb8799aef89f4bc
Alice Georgina <alicewilson1@Alices-MBP.lan>
2024-09-04 16:05:42 +0100

    edited styling

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -23,38 +23,9 @@ export default function MyPortfolio() {
               </div>
               <p className="text-sm portfolio--link">
                 <a href={item.link} target="_blank">Github</a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 20 19"
-                  fill="none"
-                >
-                  <path
-                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                    stroke="currentColor"
-                    stroke-width="2.66667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <a href={item.demo
-                } target="_blank">Demo</a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 20 19"
-                  fill="none"
-                >
-                  <path
-                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                    stroke="currentColor"
-                    stroke-width="2.66667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+    
+               {item.demo ? <a href={item.demo
+                } target="_blank">Demo</a>  : ""}
               </p>
             </div>
           </div>
